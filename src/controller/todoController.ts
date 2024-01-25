625833bdaae1a03d7efdd97152e56dc069a73193
RXGabriel <gabriel2005rf@gmail.com>
2024-01-25 15:35:31 -0300

    Listing tasks

--- src/controller/todoController.ts
+++ src/controller/todoController.ts
@@ -3,7 +3,8 @@ import { Request, Response } from 'express';
 import {Todo } from '../model/Todo';
 
 export const all = async (req: Request, res: Response) => {
-    
+  const list = await Todo.findAll()
+  res.json({list})
 }
 
 export const add = async (req: Request, res: Response) => {
