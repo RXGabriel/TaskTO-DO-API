58fe76c72e32ff66983f90e107e0ee4160bb160f
RXGabriel <gabriel2005rf@gmail.com>
2024-01-25 15:44:59 -0300

    Creating a new task

--- src/controller/todoController.ts
+++ src/controller/todoController.ts
@@ -8,7 +8,15 @@ export const all = async (req: Request, res: Response) => {
 }
 
 export const add = async (req: Request, res: Response) => {
-    
+  if(req.body.title){
+    let newTodo = await Todo.create({
+      title: req.body.title,
+      done: req.body.done ? true : false
+    })
+    res.status(201).json({item: newTodo})
+  }else{
+    res.json({error: 'Dados não enviados.'})
+  }
 }
 
 export const update = async (req: Request, res: Response) => {
